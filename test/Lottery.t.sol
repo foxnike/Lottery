9a0d48063c1a166e8a40399cde8c00c3cbd24d95
foxnike <foxnike@163.com>
2023-03-21 18:59:15 +0800

    change Lottery test

--- test/Lottery.t.sol
+++ test/Lottery.t.sol
@@ -2,18 +2,22 @@
 pragma solidity ^0.8.13;
 
 import "forge-std/Test.sol";
-import "../src/Lottery.sol";
+import  {Lottery} from "../src/Lottery.sol";
 
 contract LotteryTest is Test {
     Lottery public lottery;
+    address public alice;
 
     function setup() public{
         lottery = new Lottery();
+        alice = address(128);
+        vm.deal(alice,10**18);
     }
 
     function testBuyLottery() public payable {
         // how to add ether in below transaction ?
-        lottery.buyLottery();
+        vm.prank(alice);
+        lottery.buyLottery{value: 0.02 ether}();
     }
     function testFailBuyLottery() public {
         lottery.buyLottery();
