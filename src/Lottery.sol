7dab0063777b92ce0bfc5f92426ff37d380318b4
foxnike <foxnike@163.com>
2023-03-22 19:48:16 +0800

    more implement

--- src/Lottery.sol
+++ src/Lottery.sol
@@ -2,15 +2,16 @@
 pragma solidity ^0.8.13;
 
 contract Lottery {
-    enum Lottery_state {CLOSED, OPEN, SELECT_WINNER, EXCEPTION }
+    uint256 public stateCount = 3;
+    enum LotteryState {CLOSED, OPEN, SELECT_WINNER, EXCEPTION }
     //legal state transformation is
     // CLOSED to OPEN EXCEPTION
     // OPEN to SELECT_WINNER EXCEPTION
     // SELECT_WINNER to OPEN CLOSE EXCEPTION
     // EXCEPTION to CLOSED 
-    error stateError(Lottery_state current, Lottery_state target);
+    error stateError(LotteryState current, LotteryState target);
 
-    Lottery_state public state;
+    LotteryState public state;
     address public admin;
     uint256 public basicBid = 0.01 ether;
     address public oracle;
@@ -19,10 +20,16 @@ contract Lottery {
     uint256 public account;
     address[] public candidate;
 
-    event Winner(uint256 round, address winner, uint256 prize);
+    event NewRound(string details, uint256 round);
+
+    event SelectWinner(string details, uint256 round, uint256 prize);
+
+    event produceWinner(uint256 round, address winner, uint256 prize);
+
+    event closeSystem(string details);
 
     constructor() {
-        state = Lottery_state.CLOSED;
+        state = LotteryState.CLOSED;
         admin = msg.sender;
         round = 0;
     }
@@ -34,26 +41,58 @@ contract Lottery {
     /// @return succ if this call is successful
     function stateFlow(uint256 toState) public returns(bool succ) {
         require(msg.sender == oracle,"stateFlow() is only called by oracle");
-        
-        
+        require(toState <= stateCount,"toState is an illegal index");
+        LotteryState target = LotteryState(toState);
+        try this.handleChange(state,target) {
+            succ = true;
+        }catch {
+            succ = false;
+            revert stateError(state,target);
+            //TODO need a error output
+        }
     }
+    /// @notice this function handle the process between state changes
+    /// @param current current system state variable
+    /// @param target tatget state wanted to change to
+    function handleChange(LotteryState current,LotteryState target) external {
+        if(current == LotteryState.CLOSED && target == LotteryState.OPEN) {
+            state = LotteryState.OPEN;
+            round++;
+            emit NewRound("The Lottery System is open now",round);
+        }else
+        if(current == LotteryState.OPEN && target == LotteryState.SELECT_WINNER) {
+            state = LotteryState.SELECT_WINNER;
+            emit SelectWinner("The Lottery System is selecting winner",round,candidate.length);
+            selectWinner();
+        }else
+        if(current == LotteryState.SELECT_WINNER && target == LotteryState.OPEN) {
+            delete candidate;
+            account = 0;
+            state = LotteryState.OPEN;
+            round++;
+            emit NewRound("The Lottery System is open now",round);
+        }else
+        if(current == LotteryState.SELECT_WINNER && target == LotteryState.CLOSED) {
+            delete candidate;
+            account = 0;
+            state = LotteryState.CLOSED;
+            emit closeSystem("the Lottery System has closed");
+        }else {
+            revert stateError(state,target);
+        }
 
-
-    function getState() public view returns(uint256){
-        return uint256(state);
     }
 
     // buyers call this function with some eths,
     // return a num that is the lottery count
     function buyLottery() public payable returns(uint256 count) {
-        require(state == Lottery_state.OPEN,"The Lottery system is not open");
+        require(state == LotteryState.OPEN,"The Lottery system is not open");
         count = msg.value / basicBid;
         // buy lotterys
         for(uint256 i = 0; i < count; i++){
             candidate.push(msg.sender);
         }
         account += count * basicBid;
-        // extra gas costs in user ?
         if(msg.value != count * basicBid) {
             uint256 extra = msg.value - count * basicBid;
             payable(msg.sender).transfer(extra);
@@ -63,13 +102,13 @@ contract Lottery {
     // this contract call this function at the right moment
     // return the id of chosen winner
     function selectWinner() internal returns(uint256 winnerId) {
-        require(state == Lottery_state.OPEN, "The Lottery system is not open");
-        state = Lottery_state.SELECT_WINNER;
+        require(state == LotteryState.OPEN, "The Lottery system is not open");
+        state = LotteryState.SELECT_WINNER;
         winnerId = getRandomNum(candidate.length);
+        address payable winner = payable(candidate[winnerId]);
         uint256 prize = account * 10 / 9;
-        emit Winner(round,candidate[winnerId], prize);
-        round++;
-        // begin next stage
+        winner.transfer(prize);
+        emit produceWinner(round,winner, prize);
     }
 
     function getRandomNum(uint256 total) internal returns( uint256 num) {
@@ -78,13 +117,31 @@ contract Lottery {
 
     // this function is called when this contract happens a unexpect error
     // this function will refund all base eth in last round
-    function refund() internal returns(bool isSucc) {
-
+    // this function should be called only by contract
+    function refund() internal {
+        uint256 count = 0;
+        address current = candidate[0];
+        for(uint256 i = 0; i < candidate.length; i++) {
+            if(current == candidate[i]){
+                count++;
+            }else{
+                payable(current).transfer(basicBid * count);
+                count = 1;
+                current = candidate[i];
+            }
+        }
     }
 
     function getNum(address owner) public view returns(uint256 num) {
         for(uint256 i = 0; i < candidate.length; i++) {
             if(candidate[i] == owner) num++;
         }
+    }
+
+    function getState() public view returns(uint256){
+        return uint256(state);
+    }
+    function getRound() public view returns(uint256){
+        return round;
     }
 }
